08cf5e987c6f4afc31393e27221aaa825cc2e1dd
raghav-mayank <mayank>
2020-12-04 19:47:09 +0530

    new changes

--- application/views/member/add_member.php
+++ application/views/member/add_member.php
@@ -27,11 +27,7 @@
                             </div>
                         </div>
                         <div class="col-md-6 mb-3">
-<<<<<<< HEAD
-                            <label class="label-input" for="qualification">Qualification <span class="text-danger font-weight-medium">*</span></label>
-=======
                             <label class="label-input" for="qualification">Contact Number <span class="text-danger font-weight-medium">*</span></label>
->>>>>>> square1
                             <input type="text" class="form-control" name="qualification" id="qualification" placeholder="Degree" value="" required="">
                             <div class="invalid-feedback">
                                 Valid Qualification is required.
@@ -40,71 +36,6 @@
                     </div>
                     <div class="row">
                         <div class="col-md-6 mb-3">
-<<<<<<< HEAD
-                            <label class="label-input" for="resume">Upload your Resume <span class="text-danger font-weight-medium">*</span></label>
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" name="pic" id="validatedCustomFile" required>
-                                <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
-                                <div class="invalid-feedback">valid file required</div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label class="label-input" for="state">State</label>
-                            <select class="custom-select d-block w-100" name="state" id="state" required="">
-                                <option value="" >-state-</option>
-                                <option value="Alabama" title="Alabama">Alabama</option>
-                                <option value="Alaska" title="Alaska">Alaska</option>
-                                <option value="Arizona" title="Arizona">Arizona</option>
-                                <option value="Arkansas" title="Arkansas">Arkansas</option>
-                                <option value="California" title="California">California</option>
-                                <option value="Colorado" title="Colorado">Colorado</option>
-                                <option value="Connecticut" title="Connecticut">Connecticut</option>
-                                <option value="Delaware" title="Delaware">Delaware</option>
-                                <option value="District of Columbia" title="District of Columbia">District of Columbia</option>
-                                <option value="Florida" title="Florida">Florida</option>
-                                <option value="Georgia" title="Georgia">Georgia</option>
-                                <option value="Hawaii" title="Hawaii">Hawaii</option>
-                                <option value="Hawaii" title="Idaho">Idaho</option>
-                                <option value="Illinois" title="Illinois">Illinois</option>
-                                <option value="Indiana" title="Indiana">Indiana</option>
-                                <option value="Iowa" title="Iowa">Iowa</option>
-                                <option value="Kansas" title="Kansas">Kansas</option>
-                                <option value="Kentucky" title="Kentucky">Kentucky</option>
-                                <option value="Louisiana" title="Louisiana">Louisiana</option>
-                                <option value="Maine" title="Maine">Maine</option>
-                                <option value="Maryland" title="Maryland">Maryland</option>
-                                <option value="Massachusetts" title="Massachusetts">Massachusetts</option>
-                                <option value="Michigan" title="Michigan">Michigan</option>
-                                <option value="Minnesota" title="Minnesota">Minnesota</option>
-                                <option value="Mississippi" title="Mississippi">Mississippi</option>
-                                <option value="Missouri" title="Missouri">Missouri</option>
-                                <option value="Montana" title="Montana">Montana</option>
-                                <option value="Nebraska" title="Nebraska">Nebraska</option>
-                                <option value="Nevada" title="Nevada">Nevada</option>
-                                <option value="New Hampshire" title="New Hampshire">New Hampshire</option>
-                                <option value="New Jersey" title="New Jersey">New Jersey</option>
-                                <option value="New Mexico" title="New Mexico">New Mexico</option>
-                                <option value="New York" title="New York">New York</option>
-                                <option value="North Carolina" title="North Carolina">North Carolina</option>
-                                <option value="North Dakota" title="North Dakota">North Dakota</option>
-                                <option value="Ohio" title="Ohio">Ohio</option>
-                                <option value="Oklahoma" title="Oklahoma">Oklahoma</option>
-                                <option value="Oregon" title="Oregon">Oregon</option>
-                                <option value="Pennsylvania" title="Pennsylvania">Pennsylvania</option>
-                                <option value="Rhode Island" title="Rhode Island">Rhode Island</option>
-                                <option value="South Carolina" title="South Carolina">South Carolina</option>
-                                <option value="South Dakota" title="South Dakota">South Dakota</option>
-                                <option value="Tennessee" title="Tennessee">Tennessee</option>
-                                <option value="Texas" title="Texas">Texas</option>
-                                <option value="Utah" title="Utah">Utah</option>
-                                <option value="Vermont" title="Vermont">Vermont</option>
-                                <option value="Virginia" title="Virginia">Virginia</option>
-                                <option value="Washington" title="Washington">Washington</option>
-                                
-                                <option value="West Virginia" title="West Virginia">West Virginia</option>
-                                <option value="Wisconsin" title="Wisconsin">Wisconsin</option>
-                                <option value="Wyoming" title="Wyoming">Wyoming</option>
-=======
                             <label class="label-input" for="name">Property of address <span class="text-danger font-weight-medium">*</span></label>
                             <textarea class="form-control" rows="3" name="message" id="message" placeholder="Please fill" required></textarea>
                             <div class="invalid-feedback">
@@ -142,7 +73,6 @@
                                 <option value="" title="">Connecticut</option>
                                 <option value="" title="">Delaware</option>
                               
->>>>>>> square1
                       </select>
                             <div class="invalid-feedback">
                                 Please provide a valid state.
@@ -150,15 +80,6 @@
                         </div>
                     </div>
                     <div class="row">
-<<<<<<< HEAD
-                        <div class="col-lg-12">
-                            <div class="form-group mt-2">
-                                <label class="label-input" for="message">Message <span class="text-danger font-weight-medium">*</span></label>
-                                <textarea class="form-control" rows="3" name="message" id="message" placeholder="message" required></textarea>
-                                <div class="invalid-feedback">
-                                    Please enter a message in the textarea.
-                                </div>
-=======
                     <div class="col-md-6 mb-3">
                             <label class="label-input" for="name">Remarks<span class="text-danger font-weight-medium">*</span></label>
                             <input type="text" class="form-control" name="name" id="name" placeholder="Name" value="" required="">
@@ -171,16 +92,11 @@
                             <input type="email" class="form-control" name="email" id="email" placeholder="you@example.com" required>
                             <div class="invalid-feedback">
                                 Please enter a valid email address.
->>>>>>> square1
                             </div>
                         </div>
                     </div>
                     <div class="d-flex justify-content-center mt-3">
-<<<<<<< HEAD
-                        <button class="btn btn-blue btn-sm m-auto" name="submit" type="submit">Submit</button>
-=======
                         <button class="btn btn-primary" name="submit" type="submit">Submit</button>
->>>>>>> square1
                     </div>
             </div>
             </form>
