c6508ba0859c5e57b070a394af9ffaeb3d00b8de
Aakash <aakash143.jnv@gmail.com>
2020-12-04 20:32:20 +0530

    member

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -6,20 +6,56 @@ class Member extends CI_Controller {
     public function __construct() {
         parent::__construct();
         
-        //$this->load->model('member_model');
+        $this->load->model('member_model');
 		$this->load->helper(array('form','url','html'));
 		$this->load->library(array('form_validation','session'));
     }
     
     public function index(){
         $this->load->view('templates/admin_header');
-        $this->load->view('member/total_members');
+        $query = $this->member_model->fetch_total_members();
+        $all_members = $query->result_array();
+        $total_members['total_member'] = $all_members;
+        $this->load->view('member/total_members',$total_members);
         $this->load->view('templates/admin_footer');
     }
 
     function add_member(){
         $this->load->view('templates/admin_header');
+
+        $this->form_validation->set_rules('name', 'Full name','required|min_length[5]|regex_match[/^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$/]');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|regex_match[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]');
+        $this->form_validation->set_rules('phone', 'Phone number','required|min_length[10]|max_length[12]|regex_match[/^[0]?[0-9]\d{9}$/]');
+		$this->form_validation->set_rules('alt_phone', 'Alternate Phone number','required|min_length[10]|max_length[12]|regex_match[/^[0]?[0-9]\d{9}$/]');
+		$this->form_validation->set_rules('property_address', 'Property Of Address','required');
+        $this->form_validation->set_rules('client_address', 'Client Address','required');
+        $this->form_validation->set_rules('role', 'Role','required');
+        $this->form_validation->set_rules('remark', 'Remark','required');
+        $this->form_validation->set_rules('other_info', 'Other Information','required');
+        $this->form_validation->set_rules('reference', 'Reference By','required');
+        
+        $this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+		$this->form_validation->set_message('required', 'Enter %s');
+
+		if(isset($_POST['member_submit']) && $this->form_validation->run()) { 
+            $this->member_model->member_data();
+            redirect('member');
+        }
         $this->load->view('member/add_member');
+        $this->load->view('templates/admin_footer');
+    }
+
+    function delete_member_soft_data($id){
+        $this->load->view('templates/admin_header');
+        $this->member_model->soft_delete_member($id);
+        redirect('member');
+        $this->load->view('templates/admin_footer');
+    }
+
+    function delete_member_hard_data($id){
+        $this->load->view('templates/admin_header');
+        $this->member_model->hard_delete_member($id);
+        redirect('member');
         $this->load->view('templates/admin_footer');
     }
 
--- application/views/member/add_member.php
+++ application/views/member/add_member.php
@@ -1,188 +1,100 @@
 <div class="container">
-            <div class="wrap-career " style="margin-top:110px">
-                <h2 class="font-weight-medium text-center mt-2 mb-5">Add New Member</h2>
-                <form class="needs-validation" method="post" action="enquiry.php" novalidate="">
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                            <label class="label-input" for="name">Name <span class="text-danger font-weight-medium">*</span></label>
-                            <input type="text" class="form-control" name="name" id="name" placeholder="Name" value="" required="">
-                            <div class="invalid-feedback">
-                                Valid name is required.
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label class="label-input" for="email">Email <span class="text-danger font-weight-medium">*</span></label>
-                            <input type="email" class="form-control" name="email" id="email" placeholder="you@example.com" required>
-                            <div class="invalid-feedback">
-                                Please enter a valid email address.
-                            </div>
-                        </div>
+    <div class="wrap-career " style="margin-top:110px;margin-left:50px;">
+        <h2 class="font-weight-medium text-center mt-2 mb-5">Add New Member</h2>
+        <?php echo form_open('member/add_member',array('method'=>'post','novalidate'=>"novalidate", 'class'=>'needs-validation')); ?>
+        <?php echo validation_errors(); ?> 
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label class="label-input" for="name">Full Name <span class="text-danger font-weight-medium">*</span></label>
+                    <input type="text" class="form-control" name="name" id="name">
+                    <div class="invalid-feedback">
+                        Valid Full Name is required.
                     </div>
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                            <label class="label-input" for="mobile">Phone <span class="text-danger font-weight-medium">*</span></label>
-                            <input type="tel" class="form-control" name="mobile" id="mobile" placeholder="Phone Number" value="" required="">
-                            <div class="invalid-feedback">
-                                Valid phone number is required.
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-3">
-<<<<<<< HEAD
-                            <label class="label-input" for="qualification">Qualification <span class="text-danger font-weight-medium">*</span></label>
-=======
-                            <label class="label-input" for="qualification">Contact Number <span class="text-danger font-weight-medium">*</span></label>
->>>>>>> square1
-                            <input type="text" class="form-control" name="qualification" id="qualification" placeholder="Degree" value="" required="">
-                            <div class="invalid-feedback">
-                                Valid Qualification is required.
-                            </div>
-                        </div>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="label-input" for="email">Email <span class="text-danger font-weight-medium">*</span></label>
+                    <input type="email" class="form-control" name="email" id="email">
+                    <div class="invalid-feedback">
+                        Please enter a valid email address.
                     </div>
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-<<<<<<< HEAD
-                            <label class="label-input" for="resume">Upload your Resume <span class="text-danger font-weight-medium">*</span></label>
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" name="pic" id="validatedCustomFile" required>
-                                <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
-                                <div class="invalid-feedback">valid file required</div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label class="label-input" for="state">State</label>
-                            <select class="custom-select d-block w-100" name="state" id="state" required="">
-                                <option value="" >-state-</option>
-                                <option value="Alabama" title="Alabama">Alabama</option>
-                                <option value="Alaska" title="Alaska">Alaska</option>
-                                <option value="Arizona" title="Arizona">Arizona</option>
-                                <option value="Arkansas" title="Arkansas">Arkansas</option>
-                                <option value="California" title="California">California</option>
-                                <option value="Colorado" title="Colorado">Colorado</option>
-                                <option value="Connecticut" title="Connecticut">Connecticut</option>
-                                <option value="Delaware" title="Delaware">Delaware</option>
-                                <option value="District of Columbia" title="District of Columbia">District of Columbia</option>
-                                <option value="Florida" title="Florida">Florida</option>
-                                <option value="Georgia" title="Georgia">Georgia</option>
-                                <option value="Hawaii" title="Hawaii">Hawaii</option>
-                                <option value="Hawaii" title="Idaho">Idaho</option>
-                                <option value="Illinois" title="Illinois">Illinois</option>
-                                <option value="Indiana" title="Indiana">Indiana</option>
-                                <option value="Iowa" title="Iowa">Iowa</option>
-                                <option value="Kansas" title="Kansas">Kansas</option>
-                                <option value="Kentucky" title="Kentucky">Kentucky</option>
-                                <option value="Louisiana" title="Louisiana">Louisiana</option>
-                                <option value="Maine" title="Maine">Maine</option>
-                                <option value="Maryland" title="Maryland">Maryland</option>
-                                <option value="Massachusetts" title="Massachusetts">Massachusetts</option>
-                                <option value="Michigan" title="Michigan">Michigan</option>
-                                <option value="Minnesota" title="Minnesota">Minnesota</option>
-                                <option value="Mississippi" title="Mississippi">Mississippi</option>
-                                <option value="Missouri" title="Missouri">Missouri</option>
-                                <option value="Montana" title="Montana">Montana</option>
-                                <option value="Nebraska" title="Nebraska">Nebraska</option>
-                                <option value="Nevada" title="Nevada">Nevada</option>
-                                <option value="New Hampshire" title="New Hampshire">New Hampshire</option>
-                                <option value="New Jersey" title="New Jersey">New Jersey</option>
-                                <option value="New Mexico" title="New Mexico">New Mexico</option>
-                                <option value="New York" title="New York">New York</option>
-                                <option value="North Carolina" title="North Carolina">North Carolina</option>
-                                <option value="North Dakota" title="North Dakota">North Dakota</option>
-                                <option value="Ohio" title="Ohio">Ohio</option>
-                                <option value="Oklahoma" title="Oklahoma">Oklahoma</option>
-                                <option value="Oregon" title="Oregon">Oregon</option>
-                                <option value="Pennsylvania" title="Pennsylvania">Pennsylvania</option>
-                                <option value="Rhode Island" title="Rhode Island">Rhode Island</option>
-                                <option value="South Carolina" title="South Carolina">South Carolina</option>
-                                <option value="South Dakota" title="South Dakota">South Dakota</option>
-                                <option value="Tennessee" title="Tennessee">Tennessee</option>
-                                <option value="Texas" title="Texas">Texas</option>
-                                <option value="Utah" title="Utah">Utah</option>
-                                <option value="Vermont" title="Vermont">Vermont</option>
-                                <option value="Virginia" title="Virginia">Virginia</option>
-                                <option value="Washington" title="Washington">Washington</option>
-                                
-                                <option value="West Virginia" title="West Virginia">West Virginia</option>
-                                <option value="Wisconsin" title="Wisconsin">Wisconsin</option>
-                                <option value="Wyoming" title="Wyoming">Wyoming</option>
-=======
-                            <label class="label-input" for="name">Property of address <span class="text-danger font-weight-medium">*</span></label>
-                            <textarea class="form-control" rows="3" name="message" id="message" placeholder="Please fill" required></textarea>
-                            <div class="invalid-feedback">
-                                Valid name is required.
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label class="label-input" for="email">Client Address <span class="text-danger font-weight-medium">*</span></label>
-                            <textarea class="form-control" rows="3" name="message" id="message" placeholder="Please fill" required></textarea>
-                            <div class="invalid-feedback">
-                                Please enter a valid email address.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                    <div class="col-md-6 mb-3">
-                            <label class="label-input" for="email">Any Other Information<span class="text-danger font-weight-medium">*</span></label>
-                            <input type="email" class="form-control" name="email" id="email" placeholder="you@example.com" required>
-                            <div class="invalid-feedback">
-                                Please enter a valid email address.
-                            </div>
-                        </div>
-                    
-                        <div class="col-md-6 mb-3">
-                            <label class="label-input" for="state">Role</label>
-                            <select class="custom-select d-block w-100" name="state" id="state" required="">
-                                <option value="" >-Choose-</option>
-                                <option value="" title="">Supervisors</option>
-                                <option value="" title="">Manager</option>
-                                <option value="" title="">Team Leader</option>
-                                <option value="" title="">Department Heads</option>
-                                <option value="" title="">Advisors</option>
-                                <option value="" title="">California</option>
-                                <option value="" title="">Colorado</option>
-                                <option value="" title="">Connecticut</option>
-                                <option value="" title="">Delaware</option>
-                              
->>>>>>> square1
-                      </select>
-                            <div class="invalid-feedback">
-                                Please provide a valid state.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-<<<<<<< HEAD
-                        <div class="col-lg-12">
-                            <div class="form-group mt-2">
-                                <label class="label-input" for="message">Message <span class="text-danger font-weight-medium">*</span></label>
-                                <textarea class="form-control" rows="3" name="message" id="message" placeholder="message" required></textarea>
-                                <div class="invalid-feedback">
-                                    Please enter a message in the textarea.
-                                </div>
-=======
-                    <div class="col-md-6 mb-3">
-                            <label class="label-input" for="name">Remarks<span class="text-danger font-weight-medium">*</span></label>
-                            <input type="text" class="form-control" name="name" id="name" placeholder="Name" value="" required="">
-                            <div class="invalid-feedback">
-                                Valid name is required.
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label class="label-input" for="email">Refenceby – only for sales team <span class="text-danger font-weight-medium">*</span></label>
-                            <input type="email" class="form-control" name="email" id="email" placeholder="you@example.com" required>
-                            <div class="invalid-feedback">
-                                Please enter a valid email address.
->>>>>>> square1
-                            </div>
-                        </div>
-                    </div>
-                    <div class="d-flex justify-content-center mt-3">
-<<<<<<< HEAD
-                        <button class="btn btn-blue btn-sm m-auto" name="submit" type="submit">Submit</button>
-=======
-                        <button class="btn btn-primary" name="submit" type="submit">Submit</button>
->>>>>>> square1
-                    </div>
+                </div>
             </div>
-            </form>
-        </div>
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label class="label-input" for="phone">Phone <span class="text-danger font-weight-medium">*</span></label>
+                    <input type="tel" class="form-control" name="phone" id="phone">
+                    <div class="invalid-feedback">
+                        Valid phone number is required.
+                    </div>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="label-input" for="alt_phone">Alternate Phone Number <span class="text-danger font-weight-medium">*</span></label>
+                    <input type="text" class="form-control" name="alt_phone" id="alt_phone">
+                    <div class="invalid-feedback">
+                        Valid Alternate Phone Number is required.
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label class="label-input" for="property_address">Property of address <span class="text-danger font-weight-medium">*</span></label>
+                    <textarea class="form-control" rows="3" name="property_address" id="property_address"></textarea>
+                    <div class="invalid-feedback">
+                        Valid Property of address is required.
+                    </div>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="label-input" for="client_address">Client Address <span class="text-danger font-weight-medium">*</span></label>
+                    <textarea class="form-control" rows="3" name="client_address" id="client_address"></textarea>
+                    <div class="invalid-feedback">
+                        Please enter a valid Client Address.
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label class="label-input" for="other_info">Any Other Information<span class="text-danger font-weight-medium">*</span></label>
+                    <input type="email" class="form-control" name="other_info" id="other_info">
+                    <div class="invalid-feedback">
+                        Please enter a valid Other Information.
+                    </div>
+                </div>
+            
+                <div class="col-md-6 mb-3">
+                    <label class="label-input" for="role">Role</label>
+                    <select class="custom-select d-block w-100" name="role" id="role">
+                        <option>---Select---</option>
+                        <option value="Supervisors">Supervisors</option>
+                        <option value="Manager">Manager</option>
+                        <option value="Team Leader">Team Leader</option>
+                        <option value="Department Head">Department Head</option>
+                        <option value="Advisors">Advisors</option>
+                        <option value="Sales">Sales</option>      
+                    </select>
+                    <div class="invalid-feedback">
+                        Please provide a valid role.
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label class="label-input" for="remark">Remarks<span class="text-danger font-weight-medium">*</span></label>
+                    <input type="text" class="form-control" name="remark" id="remark">
+                    <div class="invalid-feedback">
+                        Valid name is remark.
+                    </div>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="label-input" for="reference">Reference by – only for sales team <span class="text-danger font-weight-medium">*</span></label>
+                    <input type="email" class="form-control" name="reference" id="reference">
+                    <div class="invalid-feedback">
+                        Please enter a valid reference by.
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center mt-3">
+                <button class="btn btn-primary" name="member_submit" type="submit">Submit</button>
+            </div>
+        </form>
     </div>
+</div>
