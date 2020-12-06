68b017e7144a19f1973ba12dcbf4e152da1954bb
Aakash <aakash143.jnv@gmail.com>
2020-12-06 13:55:55 +0530

    changepassword

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -13,6 +13,21 @@ class Admin_model extends CI_Model {
 
     }
 
+    function change_password($new_pass){
+        $this->db->set('password', $new_pass);
+        $this->db->where('role_id', 1);
+        return $this->db->update('sq_admin');
+        
+    }
+
+    function check_old_password() {
+        $this->db->select('password');
+        $this->db->from('sq_admin');
+		$this->db->where('role_id', 1);
+		$query = $this->db->get();
+        return $query->row_array();
+    }
+
 
 
 }
