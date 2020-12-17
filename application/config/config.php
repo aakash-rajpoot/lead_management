75fee3612d8dbb2655328750c4fb1da0fabb7fee
Aakash <aakash143.jnv@gmail.com>
2020-12-17 19:12:47 +0530

    ok

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/mayank/lead_management/';
+$config['base_url'] = 'http://localhost:8080/Codeigniter/square';
 
 //http://localhost:8080/Codeigniter/square
 /*
