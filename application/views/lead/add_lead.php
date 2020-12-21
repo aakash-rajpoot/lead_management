4c71c1ec64d29b61dc9c5bcf50273925233ec3c8
Aakash <aakash143.jnv@gmail.com>
2020-12-21 17:35:54 +0530

    unit

--- application/views/lead/add_lead.php
+++ application/views/lead/add_lead.php
@@ -1,14 +1,6 @@
-<style>
-#checkboxes label{
-    padding-right:30px;
-}
-</style>
-
-
 <div class="row">
 <div class="col-lg-12">
 <div class="content-wrapper content-wrapper--with-bg">
-
     <div class="wrap-career " style="margin-top:110px;">
         <h2 class="font-weight-medium text-center mt-2 mb-5">Add New Lead</h2>
         <?=form_open('lead/add_lead',array('method'=>'post','novalidate'=>"novalidate", 'class'=>'needs-validation')); ?>
@@ -47,36 +39,17 @@
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label class="label-input">Type of units: </label>
-                    <div id="checkboxes">
-                        <label for="1rk">
-                            <input type="checkbox" name="unit[]" class="form-control" id="1rk" />1RK</label>
-                        <label for="1bhk">
-                            <input type="checkbox" name="unit[]" class="form-control" id="1bhk" />1BHK</label>
-                        <label for="2bhk">
-                            <input type="checkbox" name="unit[]" class="form-control" id="2bhk" />2BHK</label>
-                        <label for="3bhk">
-                            <input type="checkbox" name="unit[]" class="form-control" id="3bhk" />3BHK</label>
-                        <label for="4bhk">
-                            <input type="checkbox" name="unit[]" class="form-control" id="4bhk" />4BHK</label>
-                        <label for="p_house">
-                            <input type="checkbox" name="unit[]" class="form-control" id="p_house" />P.House</label>
-                        <label for="villa">
-                            <input type="checkbox" name="unit[]" class="form-control" id="villa" />Villa</label>
-                    </div>
-                </div>
-                <div class="col-md-6 mb-3">
-                <label class="label-input">Type of units: </label>
-                <div class="form-group">
-			<select id="checkbox"  multiple >						    
-				<option value="php" class="form-control" id="1rk" name="unit[]">1RK</option>
-				<option value="python" class="form-control" id="1rk" name="unit[]">1BHK</option>		
-				<option value="javascript" class="form-control" id="2bhk" name="unit[]">2BHK</option>
-				<option value="java" class="form-control" id="3bhk" name="unit[]">3BHK</option>
-				<option value="c" class="form-control" id="4bhk" name="unit[]">4BHK</option>	
-                <option value="java" class="form-control" id="p_house" name="unit[]">P.House</option>
-				<option value="c" class="form-control" id="4bhk" name="unit[]">Villa</option>				
-			</select>	
-		</div>	
+                    <div class="form-group">
+                        <select id="checkbox" multiple>	
+                            <option class="form-control" id="1rk" name="unit[]">1RK</option>
+                            <option class="form-control" id="1bhk" name="unit[]">1BHK</option>		
+                            <option class="form-control" id="2bhk" name="unit[]">2BHK</option>
+                            <option class="form-control" id="3bhk" name="unit[]">3BHK</option>
+                            <option class="form-control" id="4bhk" name="unit[]">4BHK</option>	
+                            <option class="form-control" id="p_house" name="unit[]">P.House</option>
+                            <option class="form-control" id="villa" name="unit[]">Villa</option>				
+                        </select>	
+                    </div>	
                 </div>
             </div>
 
