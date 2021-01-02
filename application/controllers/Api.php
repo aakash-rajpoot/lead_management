6415412b66a5b3b24d4c964382f8c2a287d9bc94
iamsonu88 <69747501+iamsonu88@users.noreply.github.com>
2021-01-02 16:50:45 +0530

    Update Api.php

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,19 +1,73 @@
 <?php
+require APPPATH . '/libraries/REST_Controller.php';
 
-   require APPPATH . '/libraries/REST_Controller.php';
-   //use Restserver\Libraries\REST_Controller;
-     
 class Api extends REST_Controller {
-    
-	public function index_get()
-	{
+
+	/**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function __construct() {
+       parent::__construct();
+       $this->load->database();
+    }
+
+    public function index_get() {
         $data = array("message"=>"RESTFULL API");
         $this->response($data);
-
     }
 
-    public function get_all_data(){
-        
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+	public function data_get($id = 0) {
+        if(!empty($id)) {
+            $data = $this->db->get_where("sq_lead", ['id' => $id])->row_array();
+        } else {
+            $data = $this->db->get("sq_lead")->result();
+        }
+        $this->response($data, REST_Controller::HTTP_OK);
+	}
+
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function data_post()
+    {
+        $input = $this->input->post();
+        print_r ($input);
+        exit;
+        $this->db->insert('sq_lead',$input);
+
+        $this->response(['Lead created successfully.'], REST_Controller::HTTP_OK);
     }
-    	
+
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function data_put($id)
+    {
+        $input = $this->put();
+        $this->db->update('sq_lead', $input, array('id'=>$id));
+
+        $this->response(['Lead updated successfully.'], REST_Controller::HTTP_OK);
+    }
+
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function data_delete($id) {
+        $this->db->delete('sq_lead', array('id'=>$id));
+
+        $this->response(['Lead deleted successfully.'], REST_Controller::HTTP_OK);
+    }
 }
