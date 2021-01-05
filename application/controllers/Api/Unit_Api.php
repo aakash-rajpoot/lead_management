476834238ea3e96649aec5cd0153277371ef0b6c
Aakash <aakash143.jnv@gmail.com>
2021-01-05 15:12:15 +0530

    api

--- application/controllers/Api/Unit_Api.php
+++ application/controllers/Api/Unit_Api.php
@@ -17,7 +17,37 @@ class Unit_Api extends REST_Controller {
     }
 
     function index_post(){
-        
+        $this->form_validation->set_rules('unit_type', 'Types of units','required');
+        $this->form_validation->set_rules('unit_size', 'Unit size','required');
+        $this->form_validation->set_rules('size_measure', 'Size Measure','required');
+
+        if($this->form_validation->run()) {
+            $status = $this->unit_api_model->add_units_data();
+            if($status == "1"){
+                $this->response(['Unit added successfully.'], REST_Controller::HTTP_OK);
+            }else{
+                $this->response(['Error Found.'], REST_Controller::HTTP_OK);
+            }
+        }
+    }
+
+    function index_put($id){
+        $unit = $this->put();
+        $status = $this->unit_api_model->update_unit_data($id,$unit);
+        if($status == "1"){
+            $this->response(['Unit updated successfully.'], REST_Controller::HTTP_OK);
+        }else{
+            $this->response(['Error Found.'], REST_Controller::HTTP_OK);
+        }
+    }
+
+    function index_delete($id){
+        $status = $this->unit_api_model->delete_unit_data($id);
+        if($status == "1"){
+            $this->response(['Unit deleted successfully.'], REST_Controller::HTTP_OK);
+        }else{
+            $this->response(['Error Found.'], REST_Controller::HTTP_OK);
+        }
     }
 
 }
