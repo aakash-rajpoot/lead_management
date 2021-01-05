059a739b0402ff5d7c0fd21bc8df50f8c235a8af
raghav-mayank <mayank>
2021-01-05 13:09:16 +0530

    new changes

--- application/views/templates/admin_footer.php
+++ application/views/templates/admin_footer.php
@@ -24,18 +24,7 @@ $(document).ready(function() {
 		nonSelectedText: 'Select Unit '				
 	});
 });
-</script>   
-<script>
-    $(document).ready(function(){
-        var date_input=$('input[name="date"]'); //our date input has the name "date"
-        var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-        date_input.datepicker({
-            format: 'mm/dd/yyyy',
-            container: container,
-            todayHighlight: true,
-            autoclose: true,
-        })
-    })
-</script>
+
+
 </body>
 </html>
--- application/views/templates/admin_header.php
+++ application/views/templates/admin_header.php
@@ -38,6 +38,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/js/bootstrap-multiselect.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/css/bootstrap-multiselect.css">
 <!--  -->
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">>
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 </head>
     <body class="sidebar-is-reduced">
@@ -192,5 +195,11 @@
         </div>
         </div>
         </div>
-
+        <script>
+  $( function() {
+    $( "#joining_date" ).datepicker({dateFormat:'dd-mm-yy'});
+    // $( "#joining_date" ).datepicker('show');
+    $( "#dob" ).datepicker({dateFormat:'dd-mm-yy'});
+  });
+  </script>
 
