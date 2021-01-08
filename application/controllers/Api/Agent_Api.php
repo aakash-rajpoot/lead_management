66a2a43c232a7862dadb6c22410797354e12ae91
Aakash <aakash143.jnv@gmail.com>
2021-01-08 11:32:43 +0530

    email & api

--- application/controllers/Api/Agent_Api.php
+++ application/controllers/Api/Agent_Api.php
@@ -8,77 +8,47 @@ class Agent_Api extends REST_Controller {
 
         $this->load->model('agent_api_model');
 		$this->load->helper(array('form','url','html','string'));
-		$this->load->library(array('form_validation','session','email'));
+        $this->load->library(array('form_validation','session','email'));
     }
 
-	public function index_get() {
-        $email = $this->input->get('email');
+	public function index_post($action = '') {
+        switch($action) {
+            case 'send_otp':
+                $this->send_otp();
+                break;
+               
+            case 'verify_otp':
+                $this->verify_otp();
+                break;
+                
+            case 'create_password':
+                $this->create_password();
+                break; 
+
+            case 'change_password':
+                $this->change_password();
+                break;    
+            }
+    }
+
+    public function send_otp(){
+        $email = $this->input->post('email');
 
         if(!empty($email)) {
-            $data = $this->agent_api_model->agent_mobile_get($email);
-            if(!empty($data)) {
-                $this->response($data, REST_Controller::HTTP_OK);
-            } else {
-                $this->response(['Unable to locate account.'], REST_Controller::HTTP_OK);
+            $data = $this->agent_api_model->agent_mobile_post($email);
+        
+            if($data['pass'] == ''){
+                $login_otp = rand(000000,999999);
+                $this->agent_api_model->save_login_otp($login_otp,$data['id']);
+                $this->email_config($email,$login_otp);
+            }else{
+                $this->response(['You can already created your password.So please login by your password.'], REST_Controller::HTTP_OK);
             }
         } else {
             $this->response(['Please enter valid email.'], REST_Controller::HTTP_OK);
         }
-	}
-
-    public function index_post() {
-        
-        $this->form_validation->set_rules('name', 'Full name','required|min_length[2]|regex_match[/^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$/]');
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[sq_members.email]|regex_match[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]');
-        $this->form_validation->set_rules('phone', 'Phone number','required|min_length[10]|max_length[12]|regex_match[/^[0]?[0-9]\d{9}$/]');
-        // $this->form_validation->set_rules('alt_phone', 'Alternate Phone number','min_length[10]|max_length[12]|regex_match[/^[0]?[0-9]\d{9}$/]');
-        // $this->form_validation->set_rules('dob', 'Birth Date','required');
-        // $this->form_validation->set_rules('aadhar', 'Aadhar Card','required');
-        // $this->form_validation->set_rules('pan', 'Pan Card','required');
-        // $this->form_validation->set_rules('permanent', 'Permanent Address','required');
-            
-        // $this->form_validation->set_error_delimiters('<div class="php_error">', '</div>');
-        // $this->form_validation->set_message('required', '* Please enter valid %s');
-
-        // $config1['upload_path'] = './media/aadhar';
-        // $config1['allowed_types'] = 'pdf';
-        // $config1['max_size']    = '15000000';
-        // $config1['max_width'] = '1024';
-        // $config1['max_height'] = '768';
-
-        // $this->load->library('upload',$config1);
-        // $this->upload->initialize($config1);
-
-        // if (!$this->upload->do_upload('aadhar')){
-        //     $error = array('error' => $this->upload->display_errors());
-        // } else {
-        //     $_POST['aadhar'] = $this->upload->data('file_name');
-        // }
-
-        // $config2['upload_path'] = './media/pan';
-        // $config2['allowed_types'] = 'pdf';
-        // $config2['max_size']    = '15000000';
-        // $config2['max_width'] = '1024';
-        // $config2['max_height'] = '768';
-
-        // $this->load->library('upload',$config2);
-        // $this->upload->initialize($config2);
-
-        // if (!$this->upload->do_upload('pan')){
-        //     $error = array('error' => $this->upload->display_errors());
-        // } else {
-        //     $_POST['pan'] = $this->upload->data('file_name');
-        // }
-
-        if($this->form_validation->run()) {
-            $this->agent_api_model->agent_mobile_post();
-            $this->response(['Agent created successfully.'], REST_Controller::HTTP_OK);
-        }else{
-            $this->response(['Please check your agent validations.'], REST_Controller::HTTP_OK);
-        }
-
     }
-
+    
     public function index_put($id) {
         $input = $this->put();
         $status = $this->agent_api_model->agent_mobile_put($id,$input);
@@ -98,6 +68,110 @@ class Agent_Api extends REST_Controller {
         }
     }
 
+    function email_config($email,$login_otp){
+
+        $this->email->from('info@Kritak.com', 'KRITAK INFRA PVT.LTD.');
+        $this->email->reply_to('noreply@gmail.com', 'No Reply');
+        $this->email->to($email);
+        $this->email->subject('Kritak|OTP for Authentication');
+        $this->email->message('This OTP '.$login_otp.' ia valid within 10 minutes only. Use this OTP to create your password.');
+
+        if ($this->email->send()) {
+            $this->response(['Your OTP has been sent to your entered email.'], REST_Controller::HTTP_OK);
+        } else {
+            show_error($this->email->print_debugger());
+        }
+
+    }
+
+    function verify_otp(){
+        $otp = $this->input->post('otp');
+        $email = $this->input->post('email');
+
+        if(!empty($otp)){
+            $status = $this->agent_api_model->verify_save_otp($otp,$email);
+            if($status == '1'){
+                $this->verify_email($email);
+            }else{
+                $this->response(['Error Found.'], REST_Controller::HTTP_OK);
+            }
+        }  
+    }
+
+    function verify_email($email){
+
+        $this->email->from('info@Kritak.com', 'KRITAK INFRA PVT.LTD.');
+        $this->email->reply_to('noreply@gmail.com', 'No Reply');
+        $this->email->to($email);
+        $this->email->subject('Kritak|OTP Verified');
+        $this->email->message('Your OTP has been verified successfully.');
+ 
+        if ($this->email->send()) {
+            $this->response(['OTP has been verified.'], REST_Controller::HTTP_OK);
+        } else {
+            show_error($this->email->print_debugger());
+        }
+    }
+
+    function create_password(){
+        $this->form_validation->set_rules('password', 'Password', 'required|regex_match[/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/]|min_length[8]');
+		$this->form_validation->set_rules('confpass', 'Confirm Password', 'required|matches[password]');
+
+		if($this->form_validation->run()){
+            $email = $this->input->post('email');
+            $pwd = md5($this->input->post('password'));
+            $status = $this->agent_api_model->create_agent_pass($pwd,$email);
+            if($status == '1'){
+                $this->create_password_email($email);
+                $this->agent_api_model->after_password_success($email);
+            }else{
+                $this->response(['Error Found.'], REST_Controller::HTTP_OK);
+            }
+		}
+    }
+
+    function create_password_email($email){
+
+        $this->email->from('info@Kritak.com', 'KRITAK INFRA PVT.LTD.');
+        $this->email->reply_to('noreply@gmail.com', 'No Reply');
+        $this->email->to($email);
+        $this->email->subject('Kritak|Create Password');
+        $this->email->message('Your password has been created successfully. Now you can login your account by valid credentials.');
+ 
+        if ($this->email->send()) {
+            $this->response(['Your password has been created successfully.'], REST_Controller::HTTP_OK);
+        } else {
+            show_error($this->email->print_debugger());
+        }
+    }
+
+    function change_password(){
+        $email = $this->input->post('email');
+        $oldpass = $this->input->post('oldpass');
+        $this->form_validation->set_rules('oldpass', 'Old Password', 'required');
+        $this->form_validation->set_rules('newpass', 'New Password', 'required|regex_match[/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/]|min_length[8]');
+		$this->form_validation->set_rules('confpass', 'Confirm Password', 'required|matches[newpass]');
+
+		if($this->form_validation->run()){
+            $old_pass = $this->oldPassCheck($oldpass,$email);
+            if($old_pass == true){
+                $new_pass = md5($this->input->post('newpass'));
+                $status = $this->agent_api_model->change_pass($email,$new_pass);
+                if($status == '1'){
+                    $this->response(['Password has successfully changed.'], REST_Controller::HTTP_OK);
+                }else{
+                    $this->response(['Error Found.'], REST_Controller::HTTP_OK);
+                }
+            }
+        }
+    }
+
+    function oldPassCheck($oldpass,$email) {
+        $row = $this->agent_api_model->fetch_oldPass($email);
+		if($row['pass'] == md5($oldpass)){
+			return true;
+		}
+	}
     
     
 }
--- application/models/Agent_api_model.php
+++ application/models/Agent_api_model.php
@@ -6,35 +6,9 @@ class Agent_api_model extends CI_Model {
         $this->load->database();
     }
 
-    function agent_mobile_get($email) {
+    function agent_mobile_post($email) {
         return $this->db->get_where("sq_members", ['email' => $email,'status'=>'1'])->row_array();
     }
-
-    function agent_mobile_post() {
-        $agent = array(
-            'name' => $this->input->post('name'),
-            'phone' => $this->input->post('phone'),
-            'email' => $this->input->post('email'),
-            // 'alt_phone' => $this->input->post('alt_phone'),
-            // 'gender' => $this->input->post('gender'),
-            // 'dob' => $this->input->post('dob'),
-            // 'aadhar' => $this->input->post('aadhar'),
-            // 'pan' => $this->input->post('pan'),
-            // 'permanent' => $this->input->post('permanent'),
-            // 'correspondence' => $this->input->post('correspondence')
-        );
-        // if(empty($this->input->post('joining_date'))){
-        //     $agent['joining_date'] = date('Y-m-d');
-        // }else{
-        //     $agent['joining_date'] = $this->input->post('joining_date');
-        // }
-
-        return $this->db->insert('sq_members',$agent);
-    }
-
-    // function fetch_agent_mobile_data($id){
-    //     return $this->db->get_where("sq_members", ['id' => $id,'status'=>'1'])->row_array();
-    // }
 
     function agent_mobile_put($id,$input) {
         return $this->db->update('sq_members', $input, array('id'=>$id));
@@ -44,8 +18,49 @@ class Agent_api_model extends CI_Model {
         return $this->db->delete('sq_members', array('id'=>$id));
     }
 
-    // function reset_agent_password($email){
-    //     return $this->db->get_where("sq_members", ['email' => $email,'status'=>'1'])->row_array();
-    // }
+    function save_login_otp($login_otp,$id){
+        $otp = array(
+            'otp' => $login_otp,
+            'agent_id' => $id
+        );
+        return $this->db->insert('sq_otp_verification',$otp);
+    }
+
+    function verify_save_otp($otp,$email){
+        $id = $this->fetch_agent_id($email);
+        return $this->db->get_where("sq_otp_verification", ['agent_id'=>$id ,'otp'=>$otp, 'status'=>'0'])->num_rows();
+    }
+
+    function fetch_agent_id($email){
+        $this->db->select('id');
+        $this->db->from('sq_members'); 
+        $this->db->where('email',$email);
+        $this->db->where('status',1);
+        $query = $this->db->get();
+        return implode('',$query->row_array());
+    }
+
+    function create_agent_pass($pwd,$email){
+        return $this->db->update('sq_members', ['pass'=>$pwd], array('email'=>$email,'status'=>'1'));
+    }
+
+    function after_password_success($email){
+        $id = $this->fetch_agent_id($email);
+        return $this->db->update('sq_otp_verification', ['status'=>'1'], array('agent_id'=>$id));
+    }
     
+    function fetch_oldPass($email){
+        $this->db->select('pass');
+        $this->db->from('sq_members'); 
+        $this->db->where('email',$email);
+        $this->db->where('status',1);
+        $query = $this->db->get();
+        return $query->row_array();
+    }
+
+    function change_pass($email,$new_pass){
+        $this->db->set('pass', $new_pass);
+        $this->db->where('email', $email);
+        return $this->db->update('sq_members');
+    }
 }
