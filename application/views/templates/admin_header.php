db48b4a27e9700f1a5ade08752809069ee3b308a
raghav-mayank <mayank>
2021-01-21 21:39:08 +0530

    new

--- application/views/templates/admin_header.php
+++ application/views/templates/admin_header.php
@@ -14,19 +14,12 @@
     <!-- custom css-->
     <link rel="stylesheet" href="http://square-lead.kanvan.in/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
-    <!-- <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css"
-    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script> -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css"
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
     <script src='https://use.fontawesome.com/2188c74ac9.js'></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     
-
-    <!--  -->
-    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.2/css/jquery.dataTables.css" />
-<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css" />
-<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.2/css/jquery.dataTables_themeroller.css" />
-<script src="//cdn.datatables.net/1.10.2/js/jquery.dataTables.js"></script>
-<script src="//cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"> </script>
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script> -->
     <!-- Latest compiled and minified CSS -->
