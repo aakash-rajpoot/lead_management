23ec7c791c187494e84715227189bb307d58da1e
raghav-mayank <mayank>
2021-02-07 12:04:11 +0530

    chat -design

--- application/views/templates/admin_footer.php
+++ application/views/templates/admin_footer.php
@@ -1,18 +1,3 @@
-<style>
-    .copiyright-link {
-      color: #3e4095;
-      font-weight: bold!important;
-  }
-    </style>
-<div class="content-wrapper content-wrapper--with-bg">
-<div class="row">
-<div class="col-lg-12">
-    <div class="copyright text-center">
-        <p title="Kritak Infra Pvt. Ltd">Copyright © 2020. <a href="http://www.kritak.com/" class="copiyright-link" target="blank">Kritak Infra Pvt. Ltd.</a>. All rights reserved.</p>
-        <p title="Kanvan Business Solutions">Powered By <a href="http://kanvan.in/" class="copiyright-link" target="blank">Kanvan Business Solutions Pvt. Ltd.</a></p>
-    </div>
-</div>
-</div>
-</div>
+
  </body>
 </html>
